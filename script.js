6c4f6026503274240f37f172bc3343e233b662e5
gabriel <olaniyi_g@hotmail.com>
2021-11-03 03:16:46 +0100

    refactor: requirement adjustments
    in this commit:
    
    - the title in placed in the head
    - input rane is increased to 20 and default pint changed
    - alert is included and test is changed to "enter a positive number"

--- script.js
+++ script.js
@@ -2,15 +2,18 @@ function compute() {
     const year = new Date().getFullYear();
     let rate = document.getElementById("rate").value;
     const years = document.getElementById("years").value;
-    const principal = document.getElementById("principal").value;
+    const principal = document.getElementById("principal");
+    const principal_value = principal.value
 
-    const interest = Number((principal * years * rate) / 100);
+    const interest = Number((principal_value * years * rate) / 100);
     const maturityYear = Number(years) + Number(year);
 
-    const output = "If you deposit " + principal + "," + "<br>at an interest rate of " + rate + "%.<br>You will receive an amount of " + interest + "," + "<br> at year " + maturityYear + ".<br>";
+    const output = "If you deposit " + principal_value + "," + "<br>at an interest rate of " + rate + "%.<br>You will receive an amount of " + interest + "," + "<br> at year " + maturityYear + ".<br>";
 
-    if (principal <= 0) {
-        document.getElementById("result").innerHTML = "Please enter a valid amount";
+    if (principal_value <= 0) {
+        alert("enter a positive number")
+        document.getElementById("result").innerHTML = "Please enter a positive number";
+        principal.focus();
     } else {
         document.getElementById("result").innerHTML = output;
     }
